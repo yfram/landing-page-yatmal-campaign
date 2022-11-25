18b71481a3c635143f9001d217c65ab656ec5e78
Alan Rubin <alan@frubin.com>
2022-11-25 10:46:08 +0200

    AppBar adjustments

--- src/Elements/AppBar.js
+++ src/Elements/AppBar.js
@@ -3,41 +3,50 @@ import { Async } from "react-async";
 import getTotalAmountDonated from "../Helpers/getTotalAmountDonated";
 
 const getAmountDonated = async () => {
-    let ret = await getTotalAmountDonated();
-    return ret;
-}
-
+  let ret = await getTotalAmountDonated();
+  return ret;
+};
 
 const AppBar = () => {
-    return <Async promiseFn={getAmountDonated}>
-        {({ data, error, isLoading }) => {
-            if (isLoading) return "Loading...";
-            if (error) return `Something went wrong: ${error.message}`;
-            if (data)
-                return (
-                    <div className="app-bar">
-                        <img style={{
-                            height: "120%",
-                            float: "left",
-                        }} src={require("./../assets/images/logo-main.png")} alt="TOM logo" />
-                        <img style={{
-                            height: "90%",
-                            float: "right",
-                            marginTop: "15px",
-                            marginRight: "15px"
-                        }} src={require("./../assets/images/logo-givechack.png")} alt="Givechack logo" />
-                        <h1 className="title hebrew-text">
-                            הושג עד כה:
-                        </h1>
-                        <h1 className="big-title hebrew-text">
-                            {data.toLocaleString()} ₪
-                        </h1>
-                    </div>
-                );
-            return null;
-        }}
+  return (
+    <Async promiseFn={getAmountDonated}>
+      {({ data, error, isLoading }) => {
+        if (isLoading) return "Loading...";
+        if (error) return `Something went wrong: ${error.message}`;
+        if (data)
+          return (
+            <div className="app-bar">
+              <div>
+                <img
+                  style={{
+                    height: "100px",
+                  }}
+                  src={require("./../assets/images/logo-main.png")}
+                  alt="TOM logo"
+                />
+              </div>
+              <div>
+                <h1 className="title hebrew-text">הושג עד כה:</h1>
+                <h1 className="big-title hebrew-text">
+                  {data.toLocaleString()} ₪
+                </h1>
+              </div>
+              <div>
+                <img
+                  style={{
+                    height: "100px",
+                    margin: "10px 0",
+                  }}
+                  src={require("./../assets/images/logo-givechack.png")}
+                  alt="Givechack logo"
+                />
+              </div>
+            </div>
+          );
+        return null;
+      }}
     </Async>
-}
+  );
+};
 
 export default AppBar;
-
